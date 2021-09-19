9a7fd24ffde6f24ca3be846d4f3a18d25584fccf
hunseok-Y <81805012+hunseok-Y@users.noreply.github.com>
2021-09-20 00:46:56 +0900

    html 수정 및 css 수정
    html,css 수정 swiper 적용

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js";
 
 const searchEl = document.querySelector(".header__search");
 const searchInputEl = searchEl.querySelector(".input");
@@ -17,73 +18,25 @@ searchInputEl.addEventListener("blur", () => {
   searchInputEl.setAttribute("placeholder", "");
 });
 
-// 지정한 시간에 설정하는 함수
-// 버튼 누르면 이벤트 베너 넘어가는 코드
-const visBtn1 = document.querySelector(".visual__btn-1");
+// swiper js 코드
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+  speed: 1500,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
 
-visBtn1.addEventListener("click", () => {
-  document.querySelector(".visual__banner").style.transform =
-    "translate(100vw)";
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
-const visBtn2 = document.querySelector(".visual__btn-2");
-
-visBtn2.addEventListener("click", () => {
-  document.querySelector(".visual__banner").style.transform =
-    "translate(-100vw)";
-});
-
-// top-up
-// const dm = document.documentElement;
-// const topUp = document.querySelector(".top-up");
-
-// const documentHeight = dm.scrollHeight;
-
-// window.addEventListener("scroll", function () {
-//   let scrollToTop = dm.scrollTop;
-
-//   if (documentHeight != 0) {
-//     const actionHeight = documentHeight / 4;
-//     if (scrollToTop > actionHeight) {
-//       topUp.classList.add("visible");
-//     } else {
-//       topUp.classList.remove("visible");
-//     }
-//   }
-// });
-
-// topUp.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   scrollUp();
-// });
-
-// function scrollUp() {
-//   let upper = setInterval(function () {
-//     if (dm.scrollTop != 0) {
-//       window.scrollBy(0, -55);
-//     } else {
-//       clearInterval(upper);
-//     }
-//   }, 10);
-// }
-
-// const home = document.querySelector(".logo__text");
-// const homeHeight = home.getBoundingClientRect().height;
-
-// const topUp = document.querySelector(".top-up");
-// document.addEventListener("scroll", () => {
-//   if (window.scrollY > homeHeight / 2) {
-//     topUp.classList.add("visible");
-//   } else {
-//     topUp.classList.remove("visible");
-//   }
-// });
-
-// topUp.addEventListener("click", () => {
-//   scrollIntoview("#header");
-// });
-
-const topup = document.querySelector(".topup");
-function topup() {
-  window.scrollTo(0, 0);
-}
